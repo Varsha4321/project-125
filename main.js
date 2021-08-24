a756b9274869caf2b58c09077a4568d89461cb5c
Varsha4321 <79386531+Varsha4321@users.noreply.github.com>
2021-08-23 18:22:17 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX = 0;
+noseY = 0;
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log("poseNet is initialized")
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+        
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+    }
+}
+function draw(){
+    background('#969A97');
+    document.getElementById("text_size").innerHTML = "width and height of this square will be = " + difference + "px";
+    textSize(difference)
+    fill('#F90093');
+    stroke('#F90093');
+    text('Varsha', noseX, noseY)
+}
